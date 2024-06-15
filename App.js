50e24d5eed71353a0760ad699cb37f9e899178ab
seitann-game <seikeigakkouyou2003@gmail.com>
2024-06-16 00:04:51 +0900

    テスト４

--- App.js
+++ App.js
@@ -1,25 +1,31 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          リアクト
+class App extends React.Component{
+
+
+  render(){
+    console.log("ひつじ仙人")
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            リアクト
         
-        </a>
-      </header>
-    </div>
-  );
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
